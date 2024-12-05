336105f7b22c387813e93117a249cfa1c9c89f8c
Yann <yg.gicquel@gmail.com>
2024-12-05 15:37:56 +0100

    HashRouter  au lieu de BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Layout/Header/Header.'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';  // Changez ici
+import Header from './components/Layout/Header/Header';
 import Footer from './components/Layout/Footer/Footer';
 import Home from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
