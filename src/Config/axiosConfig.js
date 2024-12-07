c0a373e179b03443bced197af269eb01cc848b2e
Yann <yg.gicquel@gmail.com>
2024-12-07 14:12:54 +0100

    Changement de la routes axiosConfig pour communiquer avec le serveur.

--- src/Config/axiosConfig.js
+++ src/Config/axiosConfig.js
@@ -1,20 +1,23 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Base URL de l'API
+  baseURL: 'https://cleanback.fly.dev/api', // Nouvelle URL du backend
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('userToken');
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('userToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default axiosInstance;
